b41648c17ce3c2d903a5cf72b19a3f3a345ac058
Τ Ι Λ ϟ Η <MrCarb0n@proton.me>
2023-11-22 00:35:42 +0600

    🔁 | Update release! `v1.5`
     - Update typos in README.md
     - Update code formation in customization.sh
     - Enabled auto exec each 3 hours
     - Update extension
    
    Signed-off-by: Τ Ι Λ ϟ Η <MrCarb0n@proton.me>

--- customize.sh
+++ customize.sh
@@ -9,7 +9,7 @@
 # │ https://github.com/MrCarb0n/killgmsfont │
 # ╰─────────────────────────────────────────╯
 
-set -x
+# set -x
 
 ui_print ' _____ _ _ _ _____ _____ _____ _____         _   '
 ui_print '|  |  |_| | |   __|     |   __|   __|___ ___| |_ '
@@ -20,83 +20,137 @@ ui_print ''
 ui_print '- Installing'
 ui_print ''
 
-# check GMS's components state
-STATE_GMSF() {
-    ui_print '- Checking Components'
+# Check GMS's components state
+ui_print '- Checking Components'
 
-    local GMS="com.google.android.gms"
-    local GMFP="$GMS.fonts.provider.FontsProvider"
-    local GMFS="$GMS.fonts.update.UpdateSchedulerService"
+local GMS="com.google.android.gms"
+local GMFP="$GMS.fonts.provider.FontsProvider"
+local GMFS="$GMS.fonts.update.UpdateSchedulerService"
 
-    local DCL="disabledComponents:"
-    local ECL="enabledComponents:"
-    local HSP="Hidden[[:space:]]system[[:space:]]packages:"
+local DCL="disabledComponents:"
+local ECL="enabledComponents:"
+local HSP="Hidden[[:space:]]system[[:space:]]packages:"
 
-    local DATA="dumpsys package $GMS"
+local DATA="dumpsys package $GMS"
 
-    CHECK() { $DATA | sed -n "/$1/,/$2/{/$3/p}" | xargs; }
+CHECK() { $DATA | sed -n "/$1/,/$2/{/$3/p}" | xargs; }
 
-    for g in $GMFP $GMFS; do
-        case $g in
-            $(CHECK $DCL $ECL $g))
-                case $g in
-                    $GMFP) ui_print "  Provider: Disabled" ;;
-                    $GMFS) ui_print "  Service: Disabled" ;;
-                esac
-                ;;
-            $(CHECK $ECL $HSP $g))
-                case $g in
-                    $GMFP) ui_print "  Provider: Enabled" ;;
-                    $GMFS) ui_print "  Service: Enabled" ;;
-                esac
-                ;;
-        esac
-    done
-    ui_print ''
-}
+for g in $GMFP $GMFS; do
+    case $g in
+        $(CHECK $DCL $ECL $g))
+            case $g in
+                $GMFP) ui_print "  Provider: Disabled" ;;
+                $GMFS) ui_print "  Service: Disabled" ;;
+            esac
+            ;;
+        $(CHECK $ECL $HSP $g))
+            case $g in
+                $GMFP) ui_print "  Provider: Enabled" ;;
+                $GMFS) ui_print "  Service: Enabled" ;;
+            esac
+            ;;
+    esac
+done
+ui_print ''
 
 # Find GMS' generated fonts
-FIND_GMSF() {
-    ui_print '- Finding GMS Fonts'
-    local GMSFD=com.google.android.gms/files/fonts
+ui_print '- Finding GMS Fonts'
 
-    for d in /data/fonts \
-        /data/data/$GMSFD \
-        /data/user/*/$GMSFD; do
-        [ -d $d ] &&
-            ui_print "  Found: $d"
-    done
-    ui_print '  Done'
-    ui_print ''
-}
+local GMSFD=com.google.android.gms/files/fonts
 
-# cleanup $MODPATH
-CLEANUP() {
-    ui_print '- Cleaning up'
-    find $MODPATH/* -maxdepth 0 \
-        ! -name module.prop \
-        ! -name service.sh \
-        ! -name uninstall.sh -exec basename {} \; |
-        while IFS= read -r CLEAN; do
-            rm -f $MODPATH/$CLEAN
-            ui_print "  Removed: $CLEAN"
+for d in /data/fonts /data/data/$GMSFD /data/user/*/$GMSFD; do
+    [ -d $d ] && ui_print "  Found: $d"
+done
+
+ui_print '  Done'
+ui_print ''
+
+# Generating supporting files
+ui_print '- Generating Supporting Files'
+
+cat << 'EOF' > $MODPATH/service.sh
+# ╭────────────────────────────────────────────╮
+# │     Kill GMS Font Module. | @MrCarb0n      │
+# ├────────────────────────────────────────────┤
+# │   Give proper credit before distribution   │
+# │     or modification. All files licensed    │
+# │   under GNU General Public License v3.0    │
+# ├────────────────────────────────────────────┤
+# │  https://github.com/MrCarb0n/killgmsfont   │
+# ╰────────────────────────────────────────────╯
+
+# set -x
+
+while true; do
+    (
+        # Wait until proper boot up
+        until [ "$(resetprop sys.boot_completed)" = "1" -a -d "/data" ]; do
+            sleep 1
         done
-    ui_print '  Done'
-    ui_print ''
-}
 
-# settings permissions
-SET_PERM() {
-    ui_print '- Setting permissions'
-    find $MODPATH/* -maxdepth 0 \
-        -exec basename {} \; |
-        while IFS= read -r PERM; do
-            set_perm $MODPATH/$PERM 0 0 0777 u:object_r:system_file:s0
-            ui_print "  Granted: $PERM"
-        done
-    ui_print '  Done'
-    ui_print ''
-}
+        # Disable GMS' font service
+        STATE_GMSF() {
+            PM="$(command -v pm)"
+            GMSF="com.google.android.gms/com.google.android.gms.fonts"
+        
+            UPS=$(ls -d /data/user/*)  # Dynamically get user profiles
+        
+            for UP in $UPS; do
+                "$PM" "$@" --user "${UP##*/}" "$GMSF.update.UpdateSchedulerService"
+                "$PM" "$@" --user "${UP##*/}" "$GMSF.provider.FontsProvider"
+            done
+        }
 
-# run functions
-STATE_GMSF; FIND_GMSF; CLEANUP; SET_PERM
+        # Delete GMS' generated fonts
+        DEL_GMSF() {
+            local GMSFD=com.google.android.gms/files/fonts
+
+            [ -d /data/fonts ] && rm -r /data/fonts
+            
+            find /data \
+                -type d \
+                -path "*$GMSFD*" \
+                -exec test -d {} \; -exec rm -r {} \;
+        }
+
+        # Run primary tasks
+        STATE_GMSF disable >/dev/null 2>&1
+        DEL_GMSF
+    )
+
+    # Sleep for 3 hours before running again
+    sleep 10800 # 3 hours in seconds
+done
+EOF
+ui_print "  Generated: service.sh"
+
+ui_print '  Done'
+ui_print ''
+
+# Remove unnecessary files and print only the filenames
+ui_print '- Cleaning up'
+
+EXCEPTIONS="module.prop service.sh uninstall.sh"
+
+for file in $MODPATH/*; do
+    [ -e "$file" ] && [[ ! " $EXCEPTIONS " =~ " $(basename "$file") " ]] && {
+        rm -f "$file"
+        ui_print "  Removed: $(basename "$file")"
+    }
+done
+
+ui_print '  Done'
+ui_print ''
+
+# Set permissions individually for each file and print only the filenames
+ui_print '- Setting permissions'
+
+for PERM in $MODPATH/*; do
+    if [ -e "$PERM" ]; then
+        set_perm "$PERM" 0 0 0755 u:object_r:system_file:s0
+        ui_print "  Granted: $(basename "$PERM")"
+    fi
+done
+
+ui_print '  Done'
+ui_print ''
